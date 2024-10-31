c21db6d638e565991f6bd422ddd2101d785c9ec8
Andy Alvarado <andyalvarado1997@gmail.com>
2024-10-31 12:07:39 -0500

    Allow delete tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,10 @@
-"use client"
+import Tasks from "@/components/Tasks";
 
-import { useYDoc } from "@/hooks/useYDoc";
-import { useState, useEffect } from "react";
-import * as Y from "yjs"
-
-const TaskComponent = () => {
-    const { getArray } = useYDoc("tasks-room");
-    const tasks = getArray("tasks") as Y.Array<string>;
-
-    const [taskList, setTaskList] = useState(tasks.toArray());
-    const [newTask, setNewTask] = useState("");
-
-    const addTask = () => {
-        if (newTask) tasks.push([newTask]);
-        setNewTask("");
-    };
-
-    useEffect(() => {
-        const updateTaskList = () => setTaskList(tasks.toArray());
-        tasks.observe(updateTaskList);
-
-        return () => tasks.unobserve(updateTaskList);
-    }, [tasks]);
-
+export default function HomePage() {
     return (
-        <div>
-            <h2>Task example page</h2>
-            <ul>
-                {taskList.map((task, index) => (
-                    <li key={index}>{task}</li>
-                ))}
-            </ul>
-            <input
-                type="text"
-                value={newTask}
-                onChange={(e) => setNewTask(e.target.value)}
-                placeholder="Add a new task"
-            />
-            <button onClick={addTask}>Add Task</button>
+        <div className="p-3 space-y-2">
+            <h1 className="text-2xl">Next HocusPocus Server</h1>
+            <Tasks />
         </div>
     );
-};
-
-export default TaskComponent;
+}
---
+++ components/Tasks.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { useYDoc } from "@/hooks/useYDoc";
+import { useState, useEffect } from "react";
+import * as Y from "yjs"
+
+const Tasks = () => {
+    const { getArray } = useYDoc("tasks-room");
+    const tasks = getArray("tasks") as Y.Array<string>;
+
+    const [taskList, setTaskList] = useState(tasks.toArray());
+    const [newTask, setNewTask] = useState("");
+
+    const addTask = () => {
+        if (!newTask) return;
+
+        tasks.push([newTask]);
+        setNewTask("");
+    };
+
+    const removeTask = (index) => {
+        tasks.delete(index);
+    }
+
+    useEffect(() => {
+        const updateTaskList = () => setTaskList(tasks.toArray());
+        tasks.observe(updateTaskList);
+
+        return () => tasks.unobserve(updateTaskList);
+    }, [tasks]);
+
+    return (
+        <div className="space-y-4">
+            <form action={addTask} className="space-x-2 flex">
+                <input
+                    type="text"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                    placeholder="Add a new task"
+                    className="p-2 rounded text-black"
+                />
+                <button type="submit" className="rounded px-3 py-2 text-sm bg-green-500 text-white font-bold">Add Task</button>
+            </form>
+            <div>
+                <h2 className="text-lg font-bold">Tasks:</h2>
+                <ul>
+                    {
+                        !!taskList.length
+                            ? taskList.map((task, index) => (
+                                <li className="flex gap-1 items-center" key={index}>
+                                    {task}
+                                    <span className="rounded-full aspect-square cursor-pointer" onClick={() => removeTask(index)}>❌</span>
+                                </li>
+                            ))
+                            : <p>You don&apos;t have any task yet!</p>
+                    }
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
