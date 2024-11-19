7b220fc39d25a4b92e8d7c68b76d247033f77aa2
Andy Alvarado <andyalvarado1997@gmail.com>
2024-11-18 23:54:22 -0500

    Added link to y-presence page

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,9 @@ export default function HomePage() {
                 <li>
                     <Link href={'cursors'}>Cursors</Link>
                 </li>
+                <li>
+                    <Link href={'cursors'}>Cursors Y-presence and Perfect Cursors</Link>
+                </li>
             </ul>
         </div>
     );
